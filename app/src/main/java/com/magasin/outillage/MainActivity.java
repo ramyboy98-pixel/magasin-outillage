1ad1e028a506a58b48396c181f8cb0e1a1244e88
ramyboy98-pixel <ramyboy98@gmail.com>
2026-05-01 17:39:36 +0100

    Refactor MainActivity and update WebView URL
    Changed MainActivity to extend Activity instead of AppCompatActivity and updated the URL to load a local HTML file.

--- app/src/main/java/com/magasin/outillage/MainActivity.java
+++ app/src/main/java/com/magasin/outillage/MainActivity.java
@@ -1,12 +1,11 @@
 package com.magasin.outillage;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(webView);
 
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
         webView.setWebViewClient(new WebViewClient());
 
-        webView.loadUrl("https://your-github-username.github.io/magasin-outillage/");
+        webView.loadUrl("file:///android_asset/index.html");
     }
 }
