44443ff0941f407e60d122743fc167db20c860b9
ramyboy98-pixel <ramyboy98@gmail.com>
2026-05-01 17:36:24 +0100

    Implement MainActivity with WebView loading URL

---
+++ app/src/main/java/com/magasin/outillage/MainActivity.java
@@ -0,0 +1,22 @@
+package com.magasin.outillage;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://your-github-username.github.io/magasin-outillage/");
+    }
+}
